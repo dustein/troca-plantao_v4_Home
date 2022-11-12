fff925b470e6d91bb9c29698ed75c59be0372345
DuStein <steindu@gmail.com>
2022-11-12 20:59:40 -0300

    configurando algumas rotas

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,4 +6,12 @@ app.get('/', (req, res) => {
   res.status(201).send("Raiz.")
 })
 
+app.get('/users', (req, res) => {
+  res.status(200).json({message: "users list"})
+})
+
+app.post('/users', (req, res) => {
+  res.status(201).json({message: 'user created'})
+})
+
 app.listen(3030, () => { console.log('Servidor ATIVADO!')});
