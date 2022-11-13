06ca663e3406e1280b39b342c9bfa667492167d8
DuStein <steindu@gmail.com>
2022-11-13 19:40:14 -0300

    confgurada conexao mongodb

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import {PrismaClient} from '@prisma/client';
+const prisma = new PrismaClient();
 
 const app = express();
 
@@ -6,8 +8,11 @@ app.get('/', (req, res) => {
   res.status(201).send("Raiz.")
 })
 
-app.get('/users', (req, res) => {
-  res.status(200).json({message: "users list"})
+app.get('/users', async (req, res) => {
+  await prisma.$connect()
+  const users = await prisma.user.findMany();
+
+  res.status(200).json(users)
 })
 
 app.post('/users', (req, res) => {
