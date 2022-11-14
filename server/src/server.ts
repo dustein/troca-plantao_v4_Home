52b3145c7fcba286f1f6b23f0c97160953b896c2
DuStein <steindu@gmail.com>
2022-11-13 22:05:05 -0300

    problema schema job ou oferta

--- server/src/server.ts
+++ server/src/server.ts
@@ -31,6 +31,17 @@ app.post('/users', async (req, res) => {
   res.status(201).json({message: 'user created'})
 })
 
+app.post('/jobs', async (req, res) => {
+  await prisma.$connect()
+  const { dia, turno, propos } = req.body;
+  await prisma.oferta.create({
+    data: {
+      propos
 
+    }
+  })
+
+  res.status(201).json({message: 'job created'})
+})
 
 app.listen(3030, () => { console.log('Servidor ATIVADO!')});
