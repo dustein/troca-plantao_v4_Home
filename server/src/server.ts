2a56f130f07bd8a8f7073900c598ad905e2009df
DuStein <steindu@gmail.com>
2022-11-13 21:27:38 -0300

    rota create user funcionando

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import {PrismaClient} from '@prisma/client';
+
 const prisma = new PrismaClient();
 
 const app = express();
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.status(201).send("Raiz.")
@@ -12,11 +14,23 @@ app.get('/users', async (req, res) => {
   await prisma.$connect()
   const users = await prisma.user.findMany();
 
-  res.status(200).json(users)
+  res.status(200).json({cadastrados: users})
 })
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
+  await prisma.$connect()
+  const { email, nome, matricula } = req.body;
+  await prisma.user.create({
+    data: {
+      email,
+      nome,
+      matricula,
+    }
+  })
+
   res.status(201).json({message: 'user created'})
 })
+
+
 
 app.listen(3030, () => { console.log('Servidor ATIVADO!')});
