6a7d51b3a2d1d418113dea5131fcc00fd626cabf
DuStein <steindu@gmail.com>
2022-11-16 21:48:04 -0300

    estudando patch rota aceptjob

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,22 +1,52 @@
 import express from 'express';
-import {Prisma, PrismaClient} from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 const app = express();
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.status(201).send("Raiz.")
-})
 
+//lista todos users
 app.get('/users', async (req, res) => {
   await prisma.$connect()
   const users = await prisma.user.findMany();
 
   res.status(200).json({ users })
 })
+// lista todos jobs
+app.get('/jobs', async (req, res) => {
+  await prisma.$connect()
+  const jobs = await prisma.job.findMany();
 
+  res.status(200).json({ jobs })
+})
+
+//lista jobs em aberto
+app.get('/openjobs', async (req, res) => {
+  await prisma.$connect()
+  const openJobs = await prisma.job.findMany({
+    where: {
+      aceitouId: ""
+    }
+  });
+  res.status(200).json({ openJobs })
+})
+
+//lista jobs fechados
+app.get('/closedjobs', async (req, res) => {
+  await prisma.$connect()
+  const openJobs = await prisma.job.findMany({
+    where: {
+      aceitouId: {
+        not: ""
+      }
+    }
+  });
+  res.status(200).json({ openJobs })
+})
+
+//cria um user
 app.post('/users', async (req, res) => {
   await prisma.$connect()
   const { email, name, matricula } = req.body;
@@ -27,10 +57,10 @@ app.post('/users', async (req, res) => {
       matricula,
     }
   })
-
   res.status(201).json({ message: "user created."})
 })
 
+//cria um job
 app.post('/jobs', async (req, res) => {
   await prisma.$connect()
   const { date, turno, criouId, aceitouId } = req.body;
@@ -42,11 +72,29 @@ app.post('/jobs', async (req, res) => {
     criouId,
     aceitouId
   }
+
   await prisma.job.create({
     data: newJob
   })
 
   res.status(201).json({ newJob })
+})
+
+//aceita um job
+app.patch('/aceptjob', async (req, res) => {
+  const selectedJob = req.body
+  await prisma.$connect()
+  await prisma.job.update({
+    where: {
+      id: {
+      
+      }
+    },
+    data: {
+      aceitouId: "aceito"
+    }
+  })
+
 })
 
 app.listen(3030, () => { console.log('Servidor ATIVADO!')});
