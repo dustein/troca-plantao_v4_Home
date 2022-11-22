f63ef55ba6f9435e48bf8e4bda54c2073ce17474
DuStein <steindu@gmail.com>
2022-11-21 21:22:32 -0300

    css do radix complicado

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,7 @@
 import './styles/main.css'
 import * as Select from '@radix-ui/react-select';
 
+
 function App() {
 
   return (
@@ -40,13 +41,13 @@ function App() {
 
       <Select.Root>
 
-        <Select.Trigger>
+        <Select.Trigger className='flex items-center justify-center rounded py-0 px-4 text-lg h-8 gap-1.5 bg-green-700 text-red-600 shadow-2xl hover:bg-blue-400'>
           <Select.Value>Selecione o Plantão</Select.Value>
           <Select.Icon></Select.Icon>
         </Select.Trigger>
       
         <Select.Portal>
-          <Select.Content className='bg-black/20 rounded-lg shadow-md border border-cyan-600 relative top-1/2 left-1/2 flex items-center justify-items-center'>
+          <Select.Content className='overflow-hidden bg-black/20 rounded-lg shadow-md border border-cyan-600 relative top-1/2 left-1/2 flex items-center justify-items-center'>
             <Select.ScrollUpButton></Select.ScrollUpButton>
             <Select.Viewport className='p-14'>
               <Select.Item className='text-xl leading-normal text-blue-700 rounded-lg h-6 p-4 flex relative' value="SAP 1">SAP 1</Select.Item>
