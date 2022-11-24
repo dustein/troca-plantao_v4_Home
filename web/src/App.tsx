aae5ab51d605cb2421d7161c29275d3916f4ea47
DuStein <steindu@gmail.com>
2022-11-24 17:52:17 -0300

    pensando interface

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -8,10 +8,11 @@ function App() {
   return (
     <div className='flex justify-center gap-4 flex-col'>
     <h1 className='flex justify-center'>Teste</h1>
+   
 
     <Dialog.Root>
       <Dialog.Trigger>
-        <button className='text-center flex items-center justify-center border w-min p-3 m-auto border-cyan-700'>MENU</button>
+        <button className='text-center flex items-center justify-center border w-min p-3 m-auto border-cyan-700'>Cadastrar</button>
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className='bg-black/60 inset-0 fixed' />
@@ -40,13 +41,61 @@ function App() {
                 <button title="Plantão 04" className='w-8 h-8 rounded bg-zinc-800 text-zinc-400'>IV</button>
             </div>
 
-            <footer className='flex gap-4'>
-              <button className='flex gap-2 items-center justify-center border rounded-lg bg-slate-900 p-1 w-28'>
-                <XCircle />
+            <footer className='flex gap-4 justify-end mt-6'>
+              <Dialog.Close className='flex gap-2 items-center justify-center border rounded-lg bg-zinc-500 hover:bg-red-400 p-1 w-28'>
+                <XCircle className='w-6 h-6'/>
                 Cancelar
+              </Dialog.Close>
+              <button type='submit' className='flex gap-2 items-center justify-center border rounded-lg bg-emerald-900 hover:bg-emerald-600 p-1 w-28'>
+                <CheckCircle className='w-6 h-6'/>
+                Gravar
               </button>
-              <button type='submit' className='flex gap-2 items-center justify-center border rounded-lg bg-slate-900 p-1 w-28'>
-                <CheckCircle />
+
+            </footer>
+          </form>
+          <Dialog.Close />
+      </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+
+    <Dialog.Root>
+      <Dialog.Trigger>
+        <button className='text-center flex items-center justify-center border w-min p-3 m-auto border-cyan-700'>Login</button>
+      </Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay className='bg-black/60 inset-0 fixed' />
+        <Dialog.Content className='fixed bg-slate-700 py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25'>
+          <Dialog.Title className='text-3xl font-black'>Cadastro de Operacional</Dialog.Title>
+          <Dialog.Description className='text-xl'>preencha seus dados</Dialog.Description>
+          <form className='mt-2 flex flex-col gap-2'>
+            <div>
+              <label className='flex flex-col gap-2' htmlFor="username">Nome: </label>
+              <Input id="username" placeholder='seu nome de guerra'/>
+            </div>
+            <div>
+              <label className='flex flex-col gap-2' htmlFor="usermail">E-mail: </label>
+              <Input id="usermail" placeholder='seu email'/>
+            </div>
+            <div>
+              <label className='flex flex-col gap-2' htmlFor="matricula">Nome: </label>
+              <Input id="matricula" type="number" placeholder='seu ID Funcional'/>
+            </div>
+            <div className='flex gap-2  justify-evenly'>
+              <label className='flex' htmlFor="plantao">Plantão: </label>
+
+                <button title="Plantão 01" className='w-8 h-8 rounded bg-zinc-800 text-zinc-400'>I</button>
+                <button title="Plantão 02" className='w-8 h-8 rounded bg-zinc-800 text-zinc-400'>II</button>
+                <button title="Plantão 03" className='w-8 h-8 rounded bg-zinc-800 text-zinc-400'>III</button>
+                <button title="Plantão 04" className='w-8 h-8 rounded bg-zinc-800 text-zinc-400'>IV</button>
+            </div>
+
+            <footer className='flex gap-4 justify-end mt-6'>
+              <Dialog.Close className='flex gap-2 items-center justify-center border rounded-lg bg-zinc-500 hover:bg-red-400 p-1 w-28'>
+                <XCircle className='w-6 h-6'/>
+                Cancelar
+              </Dialog.Close>
+              <button type='submit' className='flex gap-2 items-center justify-center border rounded-lg bg-emerald-900 hover:bg-emerald-600 p-1 w-28'>
+                <CheckCircle className='w-6 h-6'/>
                 Gravar
               </button>
 
